6d34cfe6394dacea76992c009508cdaa7412210a
再见田园犬 <tcstory@163.com>
2022-07-30 22:28:54 +0800

    feat: 切换主题为 dracula

--- init.el
+++ init.el
@@ -32,6 +32,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (require 'init-evil)
+(load-theme 'dracula 1)
 
 (provide 'init)
 (custom-set-variables
@@ -39,7 +40,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(fcitx use-package evil)))
+ '(custom-safe-themes
+   '("73803d7cebbc240fd6cd8a54077b8fbf0b263a25db48579f5953279986283481" default))
+ '(package-selected-packages '(dracula-theme fcitx evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
