5ecb5f6d7f49870668e9802ee1b9db0fde684924
颜贵彬 <yanguibin@aomiapp.com>
2023-01-05 11:04:51 +0800

    feat: 格式化 init-evil

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,7 +6,7 @@
   :ensure t
   :config
   (setq fcitx-use-dbus nil
-      fcitx-remote-command "fcitx5-remote")
+	fcitx-remote-command "fcitx5-remote")
   (fcitx-aggressive-setup))
 
 (provide 'init-evil)
