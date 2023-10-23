b5b493242048c445e808628db4e367daee66d548
tcstory <tcstory@163.com>
2023-10-23 16:31:57 +0800

    feat: 调整配置

--- init.el
+++ init.el
@@ -36,7 +36,6 @@
 
 (require 'init-evil)
 (require 'init-doom-modeline)
-(require 'init-dirvish)
 
 (provide 'init)
 
@@ -45,7 +44,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dirvish dracula-theme use-package fcitx evil)))
+ '(package-selected-packages '(dracula-theme use-package fcitx evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
