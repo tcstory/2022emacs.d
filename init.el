8b5e492ba680e2b5ff72e0c5f44838d6eca6a99c
tcstory <tcstory@163.com>
2024-07-25 15:23:48 +0800

    feat: 替换成清华源

--- init.el
+++ init.el
@@ -16,9 +16,10 @@
 
 
 (require 'package)
-(setq package-archives '(("gnu"   . "http://1.15.88.122/gnu/")
-                         ("melpa" . "http://1.15.88.122/melpa/")))
-(package-initialize)
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
 
 ;;防止反复调用 package-refresh-contents 会影响加载速度
 (when (not package-archive-contents)
